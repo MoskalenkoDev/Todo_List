cd6731c7ca8804ca8814cb3be7b4686617ea7c1b
MoskalenkoDev <moskalenko_nikolay_dev@ukr.net>
2021-07-21 00:35:02 +0300

    added connectDB & indexServer files

---
+++ Server/src/indexServer.ts
@@ -0,0 +1,35 @@
+import express , {Application, Request, Response, NextFunction} from 'express';
+import {connectDB} from './connectDb';
+import cors from 'cors';
+
+const app : Application = express();
+
+app.use('/', (req : Request, res : Response, next : NextFunction) => {
+    next();
+});
+
+app.use(express.urlencoded({ extended: true}));
+app.use(express.json());
+app.use(cors());
+
+const PORT : string = process.env.PORT || "3001";
+
+async function start() : Promise<void> {
+    try {
+        await connectDB();
+        app.listen(PORT, ()=> {
+            console.log("Server has been started at port", PORT);
+        });
+    }
+
+    catch (e) {
+        console.log(e);
+    }
+}
+
+// app.use(userSignup);
+// app.use(userLogin);
+start();
+
+
+
